3e8fe4af15af32279fcb7b54df0232082b5e91a6
Ian K <iankoech9@gmail.com>
2020-07-17 14:30:57 +0300

    Added document selectors to the ids in html

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 var gender=document.getElementById("gender");
+var year=document.getElementById("year");
+var form=document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+    let message=[];
+    if(year.value>2020 && year.value<999){
+        message.push("Enter the appropriate year.");
+    }
+    if(message.length>0){
+        e.preventDefault();
+    }
+});
