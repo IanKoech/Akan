8a172c2b48b830687701373d3116ad3831af41bb
Ian K <iankoech9@gmail.com>
2020-07-17 19:26:08 +0300

    Set the input type to date

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var gender=document.getElementById("gender");
 var year=document.getElementById("year");
 var form=document.getElementById("form");
 var error=document.getElementById("error");
+var  day;
 
 form.addEventListener("submit",(e)=>{
     let message=[];
@@ -13,3 +14,7 @@ form.addEventListener("submit",(e)=>{
         error.innerText=message.join(" ,");
     }
 });
+
+function dayOfWeek(year){
+
+}
