aaeaa41f07c8e87e186bfc20899e9edc1a02f834
Ian K <iankoech9@gmail.com>
2020-07-18 11:55:51 +0300

    Complted syntax for js file

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ var date=document.getElementById("year");
 var year=date.getFullYear();
 var MM=date.getMonth()+1;
 var DD=date.getDate();
-var CC;
+var CC=Math.ceil(year/100);
+var YY=year-((Math.round(year/100)*100));
 var akan;
 
 
-function century(){
-    CC=Math.ceil(year/100);
-    return CC;    
-}
-
-function akanDay(){
+function akanDay(MM,CC,DD,){
     akan=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
     return akan;
 }
 
 var maleNames=["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Koffi", "Kwamw"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function getAkanName(){
+    if(gender=="male"){
+        return maleNames[akan-1];
+    }else if(gender=="female"){
+        return femaleNames[akan-1];
+    }else{
+        return "Invalid!Try again."
+    }
+}
