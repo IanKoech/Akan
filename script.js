8cb93c417fa40ebc6fd3f5520610a0914808c745
Ian K <iankoech9@gmail.com>
2020-07-18 10:28:33 +0300

    Removed action attribute from form

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var gender=document.getElementById("gender");
 var date=document.getElementById("year");
-var month=date.getMonth();
-var date=date.getDate();
-var year=date.getYear();
-var day;
- function getDay(){
-     day=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-     return day;
- }
- document.getElementById("akan").innerHTML=getDay;
+var year=date.getFullYear();
+var MM=date.getMonth()+1;
+var DD=date.getDate();
